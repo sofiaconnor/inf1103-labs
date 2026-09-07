3b4bfcb176b8c7f9aa3a83cc438c40276fe63122
Sofia <sofiaiconnor@gmail.com>
2026-09-07 13:30:11 +0800

    Dynamic Profile

--- hello.py
+++ hello.py
@@ -25,3 +25,14 @@ print("Day 2:", followers)
 
 followers += 10
 print("Day 3:", followers)
+
+# Activity 4: Interactive profile creator
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("==============================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
