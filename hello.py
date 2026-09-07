11526042aa9bb7ed48d949ec7210d72ca55356b3
Sofia <sofiaiconnor@gmail.com>
2026-09-07 13:22:49 +0800

    Assignment Variables

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+# Activity 1: Create your First Python Program and add it to the repository
 print("==============================")
 print("Welcome here")
 print("My first post!")
 print("==============================")
 
+
+# Activity 2: Update your Profile and add it to the repository
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -10,3 +13,15 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+# Activity 3: Follower Growth tracker
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
