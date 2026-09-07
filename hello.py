14a7e4bd75a0f9549098e72e612ff400a73dc0ab
Sofia <sofiaiconnor@gmail.com>
2026-09-07 13:38:56 +0800

    Fun conditions

--- hello.py
+++ hello.py
@@ -23,12 +23,12 @@ print("Day 1:", followers)
 followers += 20
 print("Day 2:", followers)
 
-followers += 10
+followers -= 10
 print("Day 3:", followers)
 
 # Activity 4: Interactive profile creator
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -36,3 +36,11 @@ print("==============================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+''' Activity 5: Something fun to think about
+- adds data type 'int' to the variable 'age'
+- adds an 'if-statement'
+'''
+
+if age > 40 and category == "fun":
+    print("You are old what is fun for you?")
